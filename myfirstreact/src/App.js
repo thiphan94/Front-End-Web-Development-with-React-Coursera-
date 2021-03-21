d245954e1426e211012c039d5459080b6e898216
Nu Uyen Thi PHAN <thiphan94@gmail.com>
2021-03-21 18:21:51 +0100

    component 2

--- myfirstreact/src/App.js
+++ myfirstreact/src/App.js
@@ -1,7 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,7 +19,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
